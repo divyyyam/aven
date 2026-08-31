3ac48183725ef6bb8813fc472023ecdb2563ea28
Divyam Singh <divyamsingh1918@gmail.com>
2026-08-31 18:22:14 +0530

    add: account, gateway and reconciliation setup

---
+++ services/reconciliation/src/test/java/com/divyam/aven/reconciliation/ReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyam.aven.reconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
